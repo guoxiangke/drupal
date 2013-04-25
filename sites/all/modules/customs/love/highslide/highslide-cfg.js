61767f918b6d8b7b21b9285b7109a7f7ec4ee44f
guoxiangke@uuke.com.cn <guoxiangke@uuke.com.cn>
2013-04-25 10:05:50 +0800

    update modules

--- sites/all/modules/customs/love/highslide/highslide-cfg.js
+++
@@ -1,5 +0,0 @@
-(function ($) {
-	hs.graphicsDir = 'sites/all/modules/customs/love/highslide/graphics/';
-	hs.outlineType = 'outer-glow';
-	hs.fullExpandPosition = 'bottom left';
-})(jQuery);
---
+++ sites/pr.yongbuzhixi.com/modules/customs/camplus_pr/camplus_pr.js
@@ -0,0 +1,36 @@
+ jQuery(function($){ // select change show notice.
+ 			$('#fgm_node_purchase_request_form_group_budgets-add-more-wrapper select').change(function(){
+			  	if($(this).parents('.multiple-inline-element').find('.multiple-field-budget-items-amount input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
+			  		// $(this).parents('.fieldset-wrapper').prepend('<div id="op-notice" class="messages warning" style="display: block; ">* Please click \'Filter\' to see your changes, and if the filter button is not here: Please click \'Refine\' to see your changes.</div>');
+			  		
+			  	}
+			});
+
+	Drupal.behaviors.pr = {
+		attach: function(context, settings) {
+			$('#fgm_node_purchase_request_form_group_budgets-add-more-wrapper select').change(function(){
+			  	if($(this).parents('.multiple-inline-element').find('.multiple-field-budget-items-amount input').attr('placeholder',Drupal.settings.camplus_pr[$(this).context.value])) {
+			  	}
+			});
+
+				$('.multiple-field-budget-items-amount input').blur(function(){
+					total = 0;
+					$('.multiple-field-budget-items-amount input').each(function(){
+						total += parseFloat($(this).val());
+					});
+					$('.field-name-field-chosen-price input').attr('value',total);
+				});
+				
+		}
+	}
+
+	$(".field-name-field-chosen-price input").attr("disabled","disabled");
+	$('.multiple-field-budget-items-amount input').blur(function(){
+		total = 0;
+		$('.multiple-field-budget-items-amount input').each(function(){
+			total += parseFloat($(this).val());
+		});
+		$('.field-name-field-chosen-price input').attr('value',total);
+	});
+
+});
